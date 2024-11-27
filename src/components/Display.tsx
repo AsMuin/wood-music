efff82b43fdcd5d982215512df58c39dd09173eb
AsMuin <asmuins@foxmail.com>
2024-11-27 11:16:24 +0000

    feat: 主题配色方案

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,5 +1,5 @@
 function Display({ ...props }) {
-    return <div className="m-2 w-[100%] overflow-auto rounded bg-[#1212112] px-6 pt-4 text-white lg:ml-0 lg:w-[75%]" {...props}></div>;
+    return <div className=" w-[100%] overflow-auto rounded bg-base px-6 pt-4 text-main lg:ml-0 lg:w-[75%]" {...props}></div>;
 }
 
 export default Display;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,18 +5,18 @@ function Navbar() {
         <>
             <div className="flex w-full items-center justify-between font-semibold">
                 <div className="flex items-center gap-2">
-                    <img className="w-8 cursor-pointer rounded-2xl bg-black p-2" src={assets.arrow_left} alt="" />
-                    <img className="w-8 cursor-pointer rounded-2xl bg-black p-2" src={assets.arrow_right} alt="" />
+                    <img className="w-8 cursor-pointer rounded-2xl bg-base p-2" src={assets.arrow_left} alt="" />
+                    <img className="w-8 cursor-pointer rounded-2xl bg-base p-2" src={assets.arrow_right} alt="" />
                 </div>
                 <div className="flex items-center gap-4">
-                    <p className="hidden cursor-pointer rounded-2xl bg-white px-4 py-1 text-[15px] text-black md:block">开通会员</p>
-                    <p className="cursor-pointer rounded-2xl bg-black px-3 py-1 text-[15px]">安装应用程序</p>
-                    <p className="flex h-7 w-7 items-center justify-center rounded-full bg-purple-500 text-black">A</p>
+                    <p className="hidden cursor-pointer rounded-2xl bg-main px-4 py-1 text-[15px] text-invert md:block hover:bg-muted hover:text-muted duration-200">开通会员</p>
+                    <p className="cursor-pointer rounded-2xl bg-base px-3 py-1 text-[15px]">安装应用程序</p>
+                    <p className="flex h-7 w-7 items-center justify-center rounded-full bg-highlight text-invert hover:scale-125 duration-500">A</p>
                 </div>
             </div>
             <div className="mt-4 flex items-center gap-2">
                 <p className="cursor-pointer rounded-2xl bg-white px-4 py-1 text-black">所有</p>
-                <p className="rounded-2xl bg-black px-4 py-1">音乐</p>
+                <p className="cursor-pointer rounded-2xl bg-base px-4 py-1">音乐</p>
             </div>
         </>
     );
